8532725e5077ff6cd0925794959f636b6a59768b
venkey <lingampally.venkey@gmail.com>
2025-12-20 23:43:17 +0530

    Test : testing CI CD

--- lib/main.dart
+++ lib/main.dart
@@ -69,7 +69,7 @@ class MyApp extends StatelessWidget {
       },
       child: MaterialApp.router(
         debugShowCheckedModeBanner: false,
-        title: 'LeetScroll-T16',
+        title: 'LeetScroll-T17',
         theme: ThemeData(
           colorScheme: ColorScheme.fromSeed(
             seedColor: Colors.green,
